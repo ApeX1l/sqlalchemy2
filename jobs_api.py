83ef857b1b5db4205f7a6aeba08472f2aab4a838
Apex <ilind663@gmail.com>
2025-03-26 23:29:46 +0300

    rest 4-5: add processor to add work | add test's for post request

--- jobs_api.py
+++ jobs_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request, request_started
 
 from data import db_session
 from data.jobs import Jobs
@@ -38,3 +38,27 @@ def get_one_news(job_id):
                                       'end_date', 'is_finished'))
         }
     )
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_job():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['team_leader', 'job', 'work_size', 'collaborators', 'start_date',
+                  'end_date', 'is_finished']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_session.global_init("db/blogs.db")
+    db_sess = db_session.create_session()
+    job = Jobs(
+        team_leader=request.json['team_leader'],
+        job=request.json['job'],
+        work_size=request.json['work_size'],
+        collaborators=request.json['collaborators'],
+        start_date=request.json['start_date'],
+        end_date=request.json['end_date'],
+        is_finished=request.json['is_finished']
+    )
+    db_sess.add(job)
+    db_sess.commit()
+    return jsonify({'id': job.id})
--- test.py
+++ test.py
@@ -1,6 +1,29 @@
-from requests import get
+from requests import post, get
+
+print(post('http://localhost:5000/api/jobs', json={}).json())  # отправляется пустая форма json
+
+print(post('http://localhost:5000/api/jobs',  # пропущен один параметр - work_size
+           json={'team_leader': 'example',
+                 'job': 'example',
+                 'collaborators': '0',
+                 'start_date': 'now',
+                 'end_date': 'never',
+                 'is_finished': False}).json())
+
+print(post('http://localhost:5000/api/jobs',  # пропущено два параметра - work_size, end_date
+           json={'team_leader': 'example',
+                 'job': 'example',
+                 'collaborators': '0',
+                 'start_date': 'now',
+                 'is_finished': False}).json())
+
+print(post('http://localhost:5000/api/jobs',  # успешное добавление работы в базу данных
+           json={'team_leader': 'example',
+                 'job': 'example',
+                 'work_size': 0,
+                 'collaborators': '0',
+                 'start_date': 'now',
+                 'end_date': 'never',
+                 'is_finished': False}).json())
 
 print(get('http://localhost:5000/api/jobs').json())
-print(get('http://localhost:5000/api/jobs/1').json())
-print(get('http://localhost:5000/api/jobs/8').json())
-print(get('http://localhost:5000/api/jobs/j').json())
