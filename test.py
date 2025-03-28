e65366aa8e5766b29826cfef2d163c68e1cdb0bc
Apex <ilind663@gmail.com>
2025-03-28 13:29:03 +0300

    rest 6-7: add delete api | test delete

--- test.py
+++ test.py
@@ -1,29 +1,5 @@
-from requests import post, get
+from requests import get, delete
 
-print(post('http://localhost:5000/api/jobs', json={}).json())  # отправляется пустая форма json
-
-print(post('http://localhost:5000/api/jobs',  # пропущен один параметр - work_size
-           json={'team_leader': 'example',
-                 'job': 'example',
-                 'collaborators': '0',
-                 'start_date': 'now',
-                 'end_date': 'never',
-                 'is_finished': False}).json())
-
-print(post('http://localhost:5000/api/jobs',  # пропущено два параметра - work_size, end_date
-           json={'team_leader': 'example',
-                 'job': 'example',
-                 'collaborators': '0',
-                 'start_date': 'now',
-                 'is_finished': False}).json())
-
-print(post('http://localhost:5000/api/jobs',  # успешное добавление работы в базу данных
-           json={'team_leader': 'example',
-                 'job': 'example',
-                 'work_size': 0,
-                 'collaborators': '0',
-                 'start_date': 'now',
-                 'end_date': 'never',
-                 'is_finished': False}).json())
-
+print(delete('http://localhost:5000/api/jobs/999').json())
+print(delete('http://localhost:5000/api/jobs/7').json())
 print(get('http://localhost:5000/api/jobs').json())
